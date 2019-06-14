43ff92cb8ffc3a4d4b4d729c8d4de0c95b146c13
DavidddM <davit.maisuradze.1@btu.edu.ge>
2019-06-14 22:56:41 +0400

    minor reconstruction

--- static/scripts/admin/productslist.js
+++ static/scripts/admin/productslist.js
@@ -63,4 +63,27 @@ window.onload = () => {
             element.style.color = "green";
         }
     });
+
+    const categories = fetch(`${document.location.origin}/api/allcats`, {
+        method: "get"
+    })
+        .then(resp => {
+            return resp.json();
+        })
+        .then(data => {
+            const select = document.querySelector("#search-select");
+            const active_category = select.getAttribute("value");
+            data.forEach(element => {
+                let option = document.createElement("option");
+                option.text = element.catName;
+                option.value = element.id;
+                if (element.id == active_category)
+                    option.setAttribute("selected", "");
+                select.appendChild(option);
+            });
+        })
+        .catch(err => {
+            console.error(err.message);
+        });
+
 };
